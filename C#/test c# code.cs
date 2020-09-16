e0dcda29831370bce7a957c1f4ec9f29662df357
Joshua Draper <josh.drapergame@gmail.com>
2020-09-15 18:29:23 -0600

    Test commit/push

--- C#/test c# code.cs
+++ C#/test c# code.cs
@@ -28,4 +28,6 @@
 //       return false
 //     }
 
+//something to commit
+
 // My mistake with the code above is lists do not have Length properties, they have Count. I also needed to else return false on line 25. There is also some syntax nonsense that I couldn't figure out so I re-wrote it.
