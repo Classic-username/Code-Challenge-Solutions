920fbcc7babb2c0e669eddf742a00912df583e70
Joshua Draper <josh.drapergame@gmail.com>
2020-10-07 21:01:41 -0600

    Bouncing Ball ACED

--- C#/codewars C# solutions.cs
+++ C#/codewars C# solutions.cs
@@ -212,6 +212,43 @@ public class Kata
   }
 }
 
+// https://www.codewars.com/kata/5544c7a5cb454edb3c000047/train/csharp
+
+// A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+// His mother looks out of a window 1.5 meters from the ground.
+
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+// Note:
+// The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+public class BouncingBall {
+	
+	public static int bouncingBall(double h, double bounce, double window) {
+		  // your code
+    int bounceCount = -1;
+	    if(h > window && 1 > bounce && bounce > 0)
+      {
+        bounceCount = 1;
+        while(h * bounce > window){
+          h = h * bounce;
+          bounceCount += 2;
+        }
+        
+      }
+      return bounceCount;
+	}
+}
+
 // Started
 
 // Planned
