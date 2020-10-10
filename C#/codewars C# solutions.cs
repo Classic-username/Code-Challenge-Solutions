468746992a4101438cd18a50c25d658658c82e28
Joshua Draper <josh.drapergame@gmail.com>
2020-10-10 14:35:08 -0600

    Longest vowel substring solved

--- C#/codewars C# solutions.cs
+++ C#/codewars C# solutions.cs
@@ -327,14 +327,48 @@ namespace Solution
         var newArr = new List<int>();
         foreach(int num in input)
         {
-                newArr.Add(num * -1);
+            newArr.Add(num * -1);
         }
         return newArr.ToArray();
     }
   }
 }
 
+// https://www.codewars.com/kata/59c5f4e9d751df43cf000035/train/csharp
 
+// The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels are any of aeiou.
+
+using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+  public static int Solve(string str)
+  {
+      var vowelList = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
+
+      var maxNum = 0;
+      var currentNum = 0;
+
+      for (int i = 0; i < str.Length; i++)
+      {
+          if (vowelList.Contains(str[i]))
+          {
+              currentNum += 1;
+              if (currentNum > maxNum)
+              {
+                  maxNum = currentNum;
+              }
+          }
+          else
+          {
+              currentNum = 0;
+          }
+
+      }
+      return maxNum;
+  }
+}
 
 // Started
 
